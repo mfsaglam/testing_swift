808d71e897a84c7d0679373206b64630e3adc38c
mfsaglam <mfsaglam@yandex.com>
2022-11-02 18:00:06 +0300

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Fatih Sağlam on 2.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
