949c5b5fb1218c1a0f17b3962d1c4cfc2953911c
mfsaglam <mfsaglam@yandex.com>
2022-11-04 23:06:37 +0300

    add lifecycleTests

---
+++ FirstTests/LifeCycleTests.swift
@@ -0,0 +1,42 @@
+//
+//  LifeCycleTests.swift
+//  FirstTests
+//
+//  Created by Fatih Sağlam on 4.11.2022.
+//
+
+import XCTest
+
+final class LifecycleTests: XCTestCase {
+    override class func setUp() {
+        print("In class setUp.")
+    }
+
+    override class func tearDown() {
+        print("In class tearDown.")
+    }
+
+    override func setUp() {
+        print("In setUp.")
+    }
+
+    override func tearDown() {
+        print("In tearDown.")
+    }
+
+    func testExample() {
+        print("Starting test.")
+
+        addTeardownBlock {
+            print("In first tearDown block.")
+        }
+
+        print("In middle of test.")
+
+        addTeardownBlock {
+            print("In second tearDown block.")
+        }
+
+        print("Finishing test.")
+    }
+}
