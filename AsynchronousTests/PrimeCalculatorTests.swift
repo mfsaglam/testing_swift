9b6ef194145292eeea6fcc267d1f16c6e7ed430c
mfsaglam <mfsaglam@yandex.com>
2022-11-09 21:06:48 +0300

    add prime calculator test

---
+++ AsynchronousTests/PrimeCalculatorTests.swift
@@ -0,0 +1,32 @@
+//
+//  PrimeCalculatorTests.swift
+//  FirstTests
+//
+//  Created by Fatih Sağlam on 9.11.2022.
+//
+
+import XCTest
+@testable import First
+
+final class PrimeCalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testPrimesUpTo100ShouldBe0() {
+        // given
+        let maximumCount = 100
+
+        // when
+        PrimeCalculator.calculate(upTo: maximumCount) {
+            // then
+            XCTAssertEqual($0.count, 0)
+        }
+    }
+
+}
