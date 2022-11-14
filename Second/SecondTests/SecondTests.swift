99906205dab57ca3b12ec52fbeeebb84a2c21c9f
mfsaglam <mfsaglam@yandex.com>
2022-11-14 16:05:28 +0300

    add test for house

--- Second/SecondTests/SecondTests.swift
+++ Second/SecondTests/SecondTests.swift
@@ -17,20 +17,18 @@ final class SecondTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test4Bed2BathHouse_Fits3Bed2BathRequirements() {
+        // given
+        let house = House(bedrooms: 4, bathrooms: 2)
+        let desiredBedrooms = 3
+        let desiredBathrooms = 2
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        // when
+        let suitability = house.checkSuitability(desiredBedrooms: desiredBedrooms, desiredBathrooms: desiredBathrooms)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // then
+        XCTAssertTrue(suitability)
     }
 
 }
