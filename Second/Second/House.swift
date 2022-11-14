37697b04a16bd653ad4d4a23ef2878e91db21a22
mfsaglam <mfsaglam@yandex.com>
2022-11-14 16:02:56 +0300

    add house struct

---
+++ Second/Second/House.swift
@@ -0,0 +1,28 @@
+//
+//  House.swift
+//  Second
+//
+//  Created by Fatih Sağlam on 14.11.2022.
+//
+
+import Foundation
+
+struct House {
+    var bedrooms: Int
+    var bathrooms: Int
+    var cost: Int
+
+    init(bedrooms: Int, bathrooms: Int) {
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.cost = bedrooms * bathrooms * 50_000
+    }
+
+    func checkSuitability(desiredBedrooms: Int, desiredBathrooms: Int) -> Bool {
+        if bedrooms >= desiredBedrooms && bathrooms >= desiredBathrooms {
+            return true
+        } else {
+            return false
+        }
+    }
+}
