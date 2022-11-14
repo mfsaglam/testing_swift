91b1ac153c8a6a8ad17aa983927022682b06ace3
mfsaglam <mfsaglam@yandex.com>
2022-11-14 15:24:35 +0300

    add tests for House

---
+++ FirstTests/HouseTests.swift
@@ -0,0 +1,34 @@
+//
+//  HouseTests.swift
+//  FirstTests
+//
+//  Created by Fatih Sağlam on 14.11.2022.
+//
+
+import XCTest
+@testable import First
+
+final class HouseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test4Bed2BathHouse_Fits3Bed2BathRequirements() {
+        // given
+        let house = House(bedrooms: 4, bathrooms: 2)
+        let desiredBedrooms = 3
+        let desiredBathrooms = 2
+
+        // when
+        let suitability = house.checkSuitability(desiredBedrooms: desiredBedrooms, desiredBathrooms: desiredBathrooms)
+
+        // then
+        XCTAssertTrue(suitability)
+    }
+
+}
