184c6d38abb4aac31c40da9e858995a6eaf95eff
mfsaglam <mfsaglam@yandex.com>
2022-11-20 20:31:25 +0300

    add buyer and house

--- First/House.swift
+++ First/House.swift
@@ -11,18 +11,32 @@ struct House {
     var bedrooms: Int
     var bathrooms: Int
     var cost: Int
-    
+
     init(bedrooms: Int, bathrooms: Int) {
         self.bedrooms = bedrooms
         self.bathrooms = bathrooms
         self.cost = bedrooms * bathrooms * 50_000
     }
-    
+
     func checkSuitability(desiredBedrooms: Int, desiredBathrooms: Int) -> Bool {
         if bedrooms >= desiredBedrooms && bathrooms >= desiredBathrooms {
             return true
         } else {
             return false
         }
+    }
+}
+
+class Buyer {
+    func view(_ house: HouseForSale) {
+        house.conductViewing()
+    }
+}
+
+class HouseForSale {
+    var numberOfViewings = 0
+
+    func conductViewing() {
+        numberOfViewings += 1
     }
 }
