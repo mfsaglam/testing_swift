f385f02c6358f052d309c86acf5dede49bff23af
mfsaglam <mfsaglam@yandex.com>
2022-11-21 22:24:15 +0300

    add power monitor test case

---
+++ FirstTests/PowerMonitorTests.swift
@@ -0,0 +1,23 @@
+//
+//  PowerMonitorTests.swift
+//  FirstTests
+//
+//  Created by Fatih Sağlam on 21.11.2022.
+//
+
+import XCTest
+@testable import First
+
+final class PowerMonitorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+
+}
