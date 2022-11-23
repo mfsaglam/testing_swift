cdc3942e81ecc0745ea950e1e95efe0c68a18495
mfsaglam <mfsaglam@yandex.com>
2022-11-23 22:46:44 +0300

    add URLSessionProtocol and make URLSession conform to it

--- First/News.swift
+++ First/News.swift
@@ -15,7 +15,7 @@ class News {
         self.url = url
     }
 
-    func fetch(using session: URLSession = URLSession.shared, completionHandler: @escaping () -> Void) {
+    func fetch(using session: URLSessionProtocol = URLSession.shared, completionHandler: @escaping () -> Void) {
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 self.stories = String(decoding: data, as: UTF8.self)
@@ -27,3 +27,9 @@ class News {
         task.resume()
     }
 }
+
+protocol URLSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol { }
