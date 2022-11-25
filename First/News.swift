e6794d457729f5127a647f06fe4ea333e00bb25a
mfsaglam <mfsaglam@yandex.com>
2022-11-25 21:36:20 +0300

    add News again

---
+++ First/News.swift
@@ -0,0 +1,27 @@
+//
+//  News.swift
+//  First
+//
+//  Created by Fatih Sağlam on 25.11.2022.
+//
+
+import Foundation
+
+class News {
+    var url: URL
+    var stories = ""
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func fetch(completionHandler: @escaping () -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                self.stories = String(decoding: data, as: UTF8.self)
+            }
+            completionHandler()
+        }
+        task.resume()
+    }
+}
