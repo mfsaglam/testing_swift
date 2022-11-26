4c04045aaf8b19b4d857d21c191a8e2b6713de7b
mfsaglam <mfsaglam@yandex.com>
2022-11-26 17:09:30 +0300

    add url session protocol and data task mock

--- First/News.swift
+++ First/News.swift
@@ -15,7 +15,7 @@ class News {
         self.url = url
     }
     
-    func fetch(using session: URLSession = URLSession.shared, completionHandler: @escaping () -> Void) {
+    func fetch(using session: URLSessionProtocol = URLSession.shared, completionHandler: @escaping () -> Void) {
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 self.stories = String(decoding: data, as: UTF8.self)
@@ -25,3 +25,25 @@ class News {
         task.resume()
     }
 }
+
+protocol URLSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol { }
+
+class DataTaskMock: URLSessionDataTask {
+    var completionHandler: (Data?, URLResponse?, Error?) -> Void
+    var resumeWasCalled = false
+    
+    // stash away the completion handler so we can call it later
+    init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        self.completionHandler = completionHandler
+    }
+    
+    override func resume() {
+        resumeWasCalled = true
+        completionHandler(nil, nil, nil)
+    }
+}
+
